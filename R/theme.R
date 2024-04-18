7026158927ed18701c32d3932661a1fa5f04b854
David Lee <david.lee@defra.gov.uk>
2024-04-18 13:19:00 +0100

    theme now adjusts default geom_line width

--- R/theme.R
+++ R/theme.R
@@ -30,6 +30,9 @@ theme_ukfsr <- function(base_size = 10,
   # instead.
   half_line <- base_size / 2
   
+  # Set default geom_line linewidth
+  update_geom_defaults("line", list(linewidth = 1))
+  
   # Throughout the theme, we use three font sizes, `base_size` (`rel(1)`)
   # for normal, `rel(0.8)` for small, and `rel(1.2)` for large.
   
