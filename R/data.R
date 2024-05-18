1c1cceb337604e786dc59f45e71ba31d38670e15
David Lee <david@lee269.co.uk>
2024-05-18 16:50:00 +0100

    Added two example datasets

---
+++ R/data.R
@@ -0,0 +1,37 @@
+#' UK production:supply ratio for food
+#' 
+#' This is an example dataset. Its helpful to have some data to play with when
+#' testing the [ukfsr] package.
+#' 
+#' @format 'prodsupply'
+#' A data frame with 134 rows and 3 columns:
+#' \describe{
+#'  \item{year}{Year}
+#'  \item{type}{Food category}
+#'  \item{value}{Production:supply ratio (%)}
+#' }
+#' @source 
+#' Agriculture in the UK:
+#' 
+#' <https://www.gov.uk/government/collections/agriculture-in-the-united-kingdom>
+#' 
+"prodsupply"
+
+
+#' Best estimates of the proportions of species threatened with extinction in
+#' the Red List Index, by species group, 2021
+#' 
+#' This is an example dataset. Its helpful to have some data to play with when
+#' testing the [ukfsr] package.
+#' 
+#' @format 'bdloss'
+#' A data frame with 15 rows and 3 columns:
+#' \describe{
+#'  \item{Type}{Species type}
+#'  \item{Status}{Threat status}
+#'  \item{Percentage}{Percentage}
+#' }
+#' @source UN Sustainable Development Goal 15
+#' 
+#' <https://unstats.un.org/sdgs/report/2021/goal-15/>
+"bdloss"
